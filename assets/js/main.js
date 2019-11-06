4cdcda5bb53cb02f424122c4ea56c6b9b7cdf95a
osahady <osahady@localhost.localdomain>
2019-11-06 16:47:46 +0300

    section optin and modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ $(function() {
             var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 
             //Put together our final background position
-            var coords = '50%' + yPos + 'px';
+            var coords = '50% ' + yPos + 'px';
 
             //move the background
             $bgobj.css({ backgroundPosition: coords });
